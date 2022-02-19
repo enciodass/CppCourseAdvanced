b605a126aa630f86840951938b735ae5535216f6
enciodass <hybrid_listed.0x@icloud.com>
2022-02-18 21:59:46 -0300

    modified:   src/main.cpp Stack instance created

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
+#include "./libary/stack.h"
 #include <iostream>
 
 int main(int argc, char **argv) {
-  std::cout << "hello world!" << std::endl;
+
+  Stack st;
+
+  char str[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
+  char *parray = str;
+
+  st.pilha(parray);
+
   return 0;
 }
